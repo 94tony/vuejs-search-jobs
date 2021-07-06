b0cfd357c63bb1afd7e646f6514afe280a343dc7
94tony <dagatatony94@gmail.com>
2021-07-06 15:14:47 +0200

    milestone 1 non finita

---
+++ js/script.js
@@ -0,0 +1,47 @@
+  
+Vue.config.devtools = true;
+
+new Vue(
+    {
+        el: '#app',
+        data: {
+            jobs: [
+                {
+                  id: 1,
+                  company: 'Perferendis',
+                  position: 'Developer',
+                  description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                  created_at: '5/22/2021',
+                  logo: 'logo.jpg',
+                  city: 'Roma',
+                  contract: 'Full Time'
+                },
+               
+                {
+                    id: 2,
+                    company: 'Microsoft',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Milano',
+                    contract: 'Full Time'
+                  },
+
+                  {
+                    id: 3,
+                    company: 'Sony',
+                    position: 'Developer',
+                    description: ' Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure veniam similique adipisci! Perferendis odio sapiente libero quam deleniti quidem consequuntur adipisci minima non iusto, sunt optio hic. Corporis, laboriosam perferendis?',
+                    created_at: '5/22/2021',
+                    logo: 'logo.jpg',
+                    city: 'Napoli',
+                    contract: 'Full Time'
+                  },      
+            ],
+            starred: [1, 2, 3],
+            applied: [4, 5]
+
+        } 
+    }
+)                   
